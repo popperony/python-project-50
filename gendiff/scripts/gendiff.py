8001144184f384ef7879191bf9ed8c609bf899a6
peronium <popperony@yandex.ru>
2023-12-27 22:35:30 +0300

    fix test error

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-from gendiff.utils.argparser import parse_args
+from gendiff.cli import parse_args
 from gendiff import generate_diff
 
 
 def main():
-    path1, path2, format = parse_args()
-    print(generate_diff(path1, path2, format))
+    path1, path2, format_ = parse_args()
+    print(generate_diff(path1, path2, format_))
 
 
 if __name__ == '__main__':
